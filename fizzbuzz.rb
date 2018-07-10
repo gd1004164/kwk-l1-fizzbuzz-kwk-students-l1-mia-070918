91d389f0232fc79386327a04def8cfa3a032b237
Gia Douglass <gigigia777@gmail.com>
2018-07-10 15:36:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 def fizzbuzz(number)
-  if ((number%3)==0)
+  if ((number%3)==0)&&((number%5)==0)
+    puts "FizzBuzz"
   elseif (number % 3)==0
     puts "Fizz"
   elsif(number%5)==0
     puts "Buzz"
+  else
+  end
 end
