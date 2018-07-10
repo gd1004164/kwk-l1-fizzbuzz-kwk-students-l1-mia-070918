1087aaa86e537334ba355f2904ff34810bf0b319
Gia Douglass <gigigia777@gmail.com>
2018-07-10 15:47:45 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(number)
+  if ((number%3)==0)&&((number%5)==0)
+    puts "FizzBuzz"
+  elsif (number % 3)==0
+    puts "Fizz"
+  elsif(number%5)==0
+    puts "Buzz"
+  else
+    puts "nil"
+  end
+end
+fizzbuzz(15)
